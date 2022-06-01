f9b3241ea193819627dbb1e8e6906375917f0a32
Richa Shahi <shahiricha2412@gmail.com>
2022-06-01 19:54:07 +0530

    complete blog post login and register pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import Home from "./pages/Home"
-import Post from "./pages/Post"
+import Home from "./pages/Home/Home"
+import Post from "./pages/Post/Post"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import ResponsiveAppBar from './components/AppBar';
-import Footeer from "./components/Footer2"
+// import Footeer from "./components/Footer2"
+import Login from "./pages/Login/Login"
+import Footeer from "./components/Footer";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
@@ -10,6 +13,8 @@ function App() {
     <ResponsiveAppBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/:id" element={<Post />} />
       </Routes>
       <Footeer />
